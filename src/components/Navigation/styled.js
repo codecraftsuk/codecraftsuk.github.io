a4f06e610de14d5d8f4a430a8243a9ac70f0f546
b zazai <baghdadzazai@yahoo.com>
2023-01-03 17:51:53 +0000

    nav bg

--- src/components/Navigation/styled.js
+++ src/components/Navigation/styled.js
@@ -9,9 +9,9 @@ export const StyledNavWrapper = styled.div`
   position: sticky;
   top: 0;
   z-index: 1000;
-  background: rgba(49, 49, 50, 0.86);
+  background: rgb(32, 34, 37);
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(5.1px);
+  backdrop-filter: blur(10.1px);
   -webkit-backdrop-filter: blur(11.1px);
 
   img {
