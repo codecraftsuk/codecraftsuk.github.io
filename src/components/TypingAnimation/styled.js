656ffdcd92aa40ae092968eed74d3c771c88664a
khushnoodasif <khushnood99@gmail.com>
2023-01-05 16:23:53 +0000

    new

--- src/components/TypingAnimation/styled.js
+++ src/components/TypingAnimation/styled.js
@@ -3,14 +3,16 @@ import styled from 'styled-components';
 
 export const StyledText = styled.div`
   overflow: hidden; /* Ensures the content is not revealed until the animation */
-  border-right: 0.15em solid red; /* The typwriter cursor */
+  border-right: 0.15em solid ${({ theme }) => theme.tertiary};; /* The typwriter cursor */
   white-space: nowrap; /* Keeps the content on a single line */
   letter-spacing: 0.15em; /* Adjust as needed */
-  animation: typing 35.5s steps(155, end), blink 0.75s step-end infinite;
-  max-width: 110px;
+  animation: typing 5s steps(155, end) 1s infinite, erase 5s steps(155, end) 2s infinite, blink 0.75s step-end infinite;
+  animation-direction: alternate;
+  max-width: 225px;
+  font-size: 30px;
   font-weight: 700;
+  color: ${({ theme }) => theme.secondary};
 
-  color: black;
   @keyframes typing {
     from {
       width: 0;
@@ -24,5 +26,14 @@ export const StyledText = styled.div`
     50% {
       border-color: transparent;
     }
+  }
+
+  @keyframes erase {
+  from {
+    width: 100%;
+  }
+  to {
+    width: 0;
+  }
   }
 `;
