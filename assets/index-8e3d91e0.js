d2091370a5f5939df5e84f2f229b7a23ae2b1c74
bz068 <bz068@users.noreply.github.com>
2023-03-05 18:05:02 +0000

    deploy: 1b3b3aa7863abb7663b507e6bc89aec89b777115

--- assets/index-8e3d91e0.js
+++ assets/index-8e3d91e0.js
@@ -1028,7 +1028,7 @@ object-assign
   width: min(90%, 1000px);
   margin: 0 auto;
   display: flex;
-  margin-top: 40px;
+  margin-top: 110px;
   position: relative;
   justify-content: space-between;
   gap: 20px;
@@ -1050,6 +1050,7 @@ object-assign
     position: absolute;
     top: -10px;
     margin-left: -15px;
+    font-size: 2.8rem;
   }
 
   .course-section-1 {
